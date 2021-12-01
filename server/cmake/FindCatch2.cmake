fe5cfd4dca86b4dcc49684cab8d1d4041953135d
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-12-01 01:02:51 +0100

    Improved CMake experience - git submodule is now updated by build system

--- server/cmake/FindCatch2.cmake
+++ server/cmake/FindCatch2.cmake
@@ -1,3 +1,15 @@
+find_package(Git REQUIRED)
+
+execute_process(
+    COMMAND ${GIT_EXECUTABLE} submodule update --init --progress
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    RESULT_VARIABLE GIT_SUBMODULE_RESULT
+)
+
+if(NOT GIT_SUBMODULE_RESULT EQUAL "0")
+    message(FATAL_ERROR "Getting submodule 'Catch2' failed with: '${GIT_SUBMODULE_RESULT}'.")
+endif()
+
 set(CATCH2_PATH ${PROJECT_SOURCE_DIR}/extern/Catch2)
 add_subdirectory(${CATCH2_PATH} "Catch2" EXCLUDE_FROM_ALL)
 list(APPEND CMAKE_MODULE_PATH ${CATCH2_PATH}/extras)
