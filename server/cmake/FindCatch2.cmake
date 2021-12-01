fec6ceef7828978eddfd5a92a701cddcfa9231e7
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-12-01 01:18:28 +0100

    Improved FindCatch2.cmake

--- server/cmake/FindCatch2.cmake
+++ server/cmake/FindCatch2.cmake
@@ -1,7 +1,7 @@
 find_package(Git REQUIRED)
 
 execute_process(
-    COMMAND ${GIT_EXECUTABLE} submodule update --init --progress
+    COMMAND ${GIT_EXECUTABLE} submodule update --init --progress "extern/Catch2"
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     RESULT_VARIABLE GIT_SUBMODULE_RESULT
 )
