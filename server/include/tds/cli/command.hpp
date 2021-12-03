463cbb03a71bf8f82fffa6b449414ff2dbf0f33c
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-12-03 01:22:12 +0100

    Changed formatting in `command` concept

--- server/include/tds/cli/command.hpp
+++ server/include/tds/cli/command.hpp
@@ -4,10 +4,13 @@
 #include <span>
 #include <string_view>
 
+// clang-format off
 namespace tds::cli {
     template<typename T>
-    concept command = std::default_initializable<T> && requires(T& com, std::span<const std::string_view> args) {
-        { T::name() } -> std::convertible_to<std::string_view>;
-        { com.execute(args) } -> std::same_as<void>;
-    };
+    concept command =
+        std::default_initializable<T> &&
+        requires(T& com, std::span<const std::string_view> args) {
+            { T::name() } -> std::convertible_to<std::string_view>;
+            { com.execute(args) } -> std::same_as<void>;
+        };
 }
