b85f3c9eb0fcf08ce5cc98b1c862bd85959160c2
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-12-05 19:58:42 +0100

    Added `SignalDevice+Epoll` tests, improved other tests

--- server/cmake/testing.cmake
+++ server/cmake/testing.cmake
@@ -1,5 +1,4 @@
 find_package(Catch2 3 QUIET)
-
 if(Catch2_FOUND)
     message(STATUS "Found Catch2: ${Catch2_DIR} (found version \"${Catch2_VERSION}\")")
 else()
@@ -15,6 +14,9 @@ else()
 
     list(APPEND CMAKE_MODULE_PATH "${Catch2_SOURCE_DIR}/extras")
 endif()
+
+find_package(Threads REQUIRED)
+set(THREADS_PREFER_PTHREAD_FLAG ON)
 
 include(CTest)
 include(Catch)
@@ -46,5 +48,5 @@ set(
 )
 
 add_executable(tds-unit-tests ${TDS_UNIT_TESTS})
-target_link_libraries(tds-unit-tests PRIVATE Catch2::Catch2WithMain tds-dev)
+target_link_libraries(tds-unit-tests PRIVATE Catch2::Catch2WithMain tds-dev Threads::Threads)
 catch_discover_tests(tds-unit-tests)
--- server/tests/unit/linux/signal_device_test.cpp
+++ server/tests/unit/linux/signal_device_test.cpp
@@ -1,22 +1,104 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include "tds/linux/epoll.hpp"
 #include "tds/linux/signal_device.hpp"
 
 #include <csignal>
+#include <thread>
 
+#include <sys/wait.h>
 #include <unistd.h>
 
 using namespace tds::linux;
 
+void test_signal_device(std::invocable auto test) {
+    const int fork_result = fork();
+
+    switch(fork()) {
+    case -1:
+        CHECK(!"`test_signal_device` failed to create process with fork(2)");
+        break;
+
+    case 0:
+        test();
+        std::exit(0);
+        break;
+
+    default:
+        wait(nullptr);
+    }
+}
+
 TEST_CASE("tds::linux::SignalDevice", "[linux]") {
-    SignalDevice signal_device;
-    bool flag = false;
+    SECTION("Test in main thread") {
+        test_signal_device([] {
+            SignalDevice signal_device;
+            bool flag = false;
 
-    signal_device.add_handler(SIGINT, [&](int) { flag = true; });
-    signal_device.apply();
+            signal_device.add_handler(SIGINT, [&](int) { flag = true; });
+            signal_device.apply();
 
-    std::raise(SIGINT);
-    signal_device.handle();
+            std::raise(SIGINT);
+            signal_device.handle();
 
-    REQUIRE(flag);
+            REQUIRE(flag);
+        });
+    }
+
+    SECTION("Test in different thread") {
+        test_signal_device([] {
+            std::thread{[] {
+                SignalDevice signal_device;
+                bool flag = false;
+
+                signal_device.add_handler(SIGINT, [&](int) { flag = true; });
+                signal_device.apply();
+
+                std::raise(SIGINT);
+                signal_device.handle();
+
+                REQUIRE(flag);
+            }}.join();
+        });
+    }
+}
+
+TEST_CASE("tds::linux::{SignalDevice + Epoll}", "[linux]") {
+    SECTION("Test in main thread") {
+        test_signal_device([] {
+            SignalDevice signal_device;
+            bool flag = false;
+
+            signal_device.add_handler(SIGINT, [&](int) { flag = true; });
+            signal_device.apply();
+
+            Epoll epoll;
+            epoll.add_device(signal_device);
+
+            std::raise(SIGINT);
+            epoll.handle();
+
+            REQUIRE(flag);
+        });
+    }
+
+    SECTION("Test in different thread") {
+        test_signal_device([] {
+            std::thread{[] {
+                SignalDevice signal_device;
+                bool flag = false;
+
+                signal_device.add_handler(SIGINT, [&](int) { flag = true; });
+                signal_device.apply();
+
+                Epoll epoll;
+                epoll.add_device(signal_device);
+
+                std::raise(SIGINT);
+                epoll.handle();
+
+                REQUIRE(flag);
+            }}.join();
+        });
+    }
 }
