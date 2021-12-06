86a6fdd252b7644ccdb76f07681c8207f559f0a4
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-12-06 19:37:15 +0100

    Added `Init` tests

--- server/tests/unit/cli/init_command_test.cpp
+++ server/tests/unit/cli/init_command_test.cpp
@@ -1,9 +1,14 @@
 #include <catch2/catch_test_macros.hpp>
+#include <unit.hpp>
 
 #include "tds/cli/command.hpp"
 #include "tds/cli/init_command.hpp"
+#include <array>
+#include <filesystem>
 
 using namespace tds::cli;
+using namespace std::string_view_literals;
+namespace fs = std::filesystem;
 
 TEST_CASE("tds::cli::InitCommand", "[cli]") {
     SECTION("Is `InitCommand` a valid command?") {
@@ -14,5 +19,23 @@ TEST_CASE("tds::cli::InitCommand", "[cli]") {
         const std::string_view expected = "init";
         REQUIRE(InitCommand::name == expected);
         REQUIRE(InitCommand::get_name() == expected);
+    }
+
+    SECTION("Test creating instance in current path") {
+        tds::unit::test_in_new_process([]{
+            fs::current_path("/tmp");
+            InitCommand init;
+            init.execute({});
+            return static_cast<int>(!fs::exists(fs::current_path() / ".tds"));
+        });
+    }
+
+    SECTION("Test creating instance in different path") {
+        tds::unit::test_in_new_process([]{
+            InitCommand init;
+            const auto args = std::to_array<std::string_view>({"/tmp"});
+            init.execute(args);
+            return static_cast<int>(!fs::exists("/tmp/.tds"));
+        });
     }
 }
