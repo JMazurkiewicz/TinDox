a3538b02edb72bba54466ecbc71eb4098c2c7747
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2021-12-09 00:55:05 +0100

    Add ability to change timeout in `EpollDevice` (#59)

--- server/include/tds/linux/epoll_device.hpp
+++ server/include/tds/linux/epoll_device.hpp
@@ -2,7 +2,6 @@
 
 #include "tds/linux/io_device.hpp"
 
-#include <chrono>
 #include <span>
 #include <unordered_map>
 
@@ -11,17 +10,17 @@
 namespace tds::linux {
     class EpollDevice : public IoDevice {
     public:
-        static constexpr std::chrono::milliseconds default_timeout{5000};
-
         EpollDevice();
 
+        void set_timeout(int new_timeout);
         void add_device(IoDevice& dev);
         void handle() override;
 
     private:
-        int get_event_count(std::span<epoll_event> events);
+        int wait_for_events(std::span<epoll_event> events);
         IoDevice& get_device(epoll_event event);
 
+        int m_timeout;
         std::unordered_map<int, IoDevice*> m_devices;
     };
 }
--- server/src/linux/epoll_device.cpp
+++ server/src/linux/epoll_device.cpp
@@ -6,12 +6,17 @@
 #include <ranges>
 
 namespace tds::linux {
-    EpollDevice::EpollDevice() {
+    EpollDevice::EpollDevice()
+        : m_timeout{-1} {
         if(const int epoll_fd = epoll_create1(0); epoll_fd != -1) {
             set_fd(epoll_fd);
         } else {
             throw LinuxError{"epoll_create1(2)"};
         }
+    }
+
+    void EpollDevice::set_timeout(int new_timeout) {
+        m_timeout = std::ranges::max(new_timeout, -1);
     }
 
     void EpollDevice::add_device(IoDevice& dev) {
@@ -26,16 +31,16 @@ namespace tds::linux {
     }
 
     void EpollDevice::handle() {
-        std::array<epoll_event, 256> events; // @todo temporary solution
-        const int event_count = get_event_count(events);
+        std::array<epoll_event, 64> events;
+        const int event_count = wait_for_events(events);
 
         auto devices = events | std::views::take(event_count) |
                        std::views::transform(std::bind_front(&EpollDevice::get_device, this));
         std::ranges::for_each(devices, &IoDevice::handle);
     }
 
-    int EpollDevice::get_event_count(std::span<epoll_event> events) {
-        if(const int count = epoll_wait(get_fd(), events.data(), events.size(), default_timeout.count()); count != -1) {
+    int EpollDevice::wait_for_events(std::span<epoll_event> events) {
+        if(const int count = epoll_wait(get_fd(), events.data(), events.size(), m_timeout); count != -1) {
             return count;
         } else {
             throw LinuxError{"epoll_wait(2)"};
