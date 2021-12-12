77138c4d406682195f74db7dfcc10caa6f567ba9
Damian Piotrowski <damianp.150898@gmail.com>
2021-12-12 23:32:01 +0100

    attempt to connect to server

--- mobile/app/src/main/java/com/example/mobileclient/MainActivity.kt
+++ mobile/app/src/main/java/com/example/mobileclient/MainActivity.kt
@@ -8,6 +8,14 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.InetAddress
+import java.net.Socket
+import java.nio.ByteBuffer
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var ipAddressEditText: EditText
@@ -15,12 +23,68 @@ class MainActivity : AppCompatActivity() {
     private lateinit var submitButton: Button
     private lateinit var resultTextView: TextView
 
-    fun submit(view: View) {
-        Toast.makeText(this, ipAddressEditText.text, Toast.LENGTH_SHORT).show()
-        val message = "message " + ipAddressEditText.text + " " + portEditText.text
-        Log.i("debug", ipAddressEditText.text.toString())
-        resultTextView.text = message
+    private var active: Boolean = false
+    private var data: String = ""
+
+    private suspend fun client( ip: String, port: Int) {
+
+        val connection = Socket(ip, port)
+        val reader = Scanner(connection.getInputStream())
+
+
+        while(active)
+        {
+            var input = ""
+            input = reader.nextLine()
+
+            if(data.length < 300)
+                data += "\n$input"
+            else
+                data = input
+
+            resultTextView.text = data
+        }
+
+        reader.close()
+        connection.close()
+
+
     }
+
+
+    fun submit(view: View) {
+        //Toast.makeText(this, ipAddressEditText.text, Toast.LENGTH_SHORT).show()
+
+
+        val address = "83.5.133.22"
+            //ipAddressEditText.text.toString()
+        val port = 37666
+            //Integer.parseInt(portEditText.text.toString())
+
+        /*var socket = Socket(ipAddressEditText.text.toString() , Integer.parseInt(portEditText.text.toString()))
+        var info = socket.getInputStream()
+        var buffer = ByteArray(128)
+        var count = info.read(buffer)*/
+
+        if(!active){
+            submitButton.text ="disconnect"
+            active = true
+            CoroutineScope(IO).launch{
+                client( address, port )
+            }
+        }else
+        {
+            active = false
+            submitButton.text = "connect"
+
+        }
+
+
+        //resultTextView.text = result
+
+    }
+
+    //val socket = Socket(InetAddress.getByAddress("83.5.133.22".toByteArray()), 37666)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
