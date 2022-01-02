69439a9e1c96ea0c5c357e5f54069c313fdb2294
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2022-01-02 17:34:51 +0100

    Implemented `name` command

--- server/include/tds/protocol/default_command_executor.hpp
+++ server/include/tds/protocol/default_command_executor.hpp
@@ -5,8 +5,9 @@
 #include "tds/protocol/execution/command_executor.hpp"
 #include "tds/protocol/execution/ls.hpp"
 #include "tds/protocol/execution/pwd.hpp"
+#include "tds/protocol/execution/name.hpp"
 
 namespace tds::protocol {
     using DefaultCommandExecutor =
-        execution::CommandExecutor<execution::Auth, execution::Cd, execution::Ls, execution::Pwd>;
+        execution::CommandExecutor<execution::Auth, execution::Cd, execution::Ls, execution::Name, execution::Pwd>;
 }
