3667b986015977e6c09456a96646a8a0f3a46688
Łukasz Reszka <74792443+LukaszReszka@users.noreply.github.com>
2022-01-22 23:40:43 +0100

    Cli (#136)
    * Small tutorial
    
    * Still fixing ...
    
    * fixed bsd layer
    
    Co-authored-by: Jakub Mazurkiewicz <mazkuba3@gmail.com>

--- cli/src/tdpservice/tdpservice.h
+++ cli/src/tdpservice/tdpservice.h
@@ -6,52 +6,54 @@
 
 class TDPService {
 public:
-	TDPService() : connectionToServer(std::make_shared<Connection>()), responseAnalyzer(connectionToServer) {}
+    TDPService() = default;
 
-	~TDPService() {
-		closeConnection();
-	}
+    ~TDPService() {
+        closeConnection();
+    }
 
-	bool initConnection(const string &serv_ip, int serv_port);
+    bool initConnection(const string &serv_ip, int serv_port);
 
-	bool closeConnection();
+    bool closeConnection();
 
-	bool logout();
+    bool logout();
 
-	bool auth(const string &login, const string &passwd);
+    bool auth(const string &login, const string &passwd);
 
-	bool mkdir(const string &name);
+    bool mkdir(const string &name);
 
-	bool rm(const string &name);
+    bool rm(const string &name);
 
-	bool rename(const string &oname, const string &nname);
+    bool rename(const string &oname, const string &nname);
 
-	bool cp(const string &name, const string &path);
+    bool cp(const string &name, const string &path);
 
-	bool mv(const string &name, const string &path);
+    bool mv(const string &name, const string &path);
 
-	bool cd(const string &path);
+    bool cd(const string &path);
 
-	bool tree(const string &path);
+    bool tree(const string &path);
 
-	bool ls(const string &path, const string &size, const string &mod);
+    bool ls(const string &path, const string &size, const string &mod);
 
-	bool name();
+    bool name();
 
-	bool pwd();
+    bool perms();
 
-	string response_body;
-	string received_response;
-	string last_sent_command;
-	ConnectionError error_code = NONE;
+    bool pwd();
+
+    string response_body;
+    string received_response;
+    string last_sent_command;
+    ConnectionError error_code = NONE;
 
 private:
-	shared_ptr<Connection> connectionToServer;
-	ResponseAnalyzer responseAnalyzer;
+    Connection connectionToServer;
+    ResponseAnalyzer responseAnalyzer;
 
-	bool sendCommand(const string &command_name, const string &field_name1, const string &field_value1,
-	                 const string &field_name2, const string &field_value2,
-	                 const string &field_name3, const string &field_value3);
+    bool sendAndGetResponse(const string &command_name, const string &field_name1, const string &field_value1,
+                            const string &field_name2, const string &field_value2,
+                            const string &field_name3, const string &field_value3);
 };
 
 
