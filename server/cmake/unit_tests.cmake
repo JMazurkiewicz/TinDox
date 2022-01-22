f837cd7f9cfaf0335c214b2301b7a368007afccd
Jakub Mazurkiewicz <mazkuba3@gmail.com>
2022-01-22 02:09:33 +0100

    CMake tweaks

--- server/cmake/unit_tests.cmake
+++ server/cmake/unit_tests.cmake
@@ -2,16 +2,13 @@ find_package(Catch2 3 QUIET)
 if(Catch2_FOUND)
     message(STATUS "Found Catch2: ${Catch2_DIR} (found version \"${Catch2_VERSION}\")")
 else()
-    include(FetchContent)
-    set(FETCHCONTENT_QUIET OFF)
-
     FetchContent_Declare(
         Catch2
         GIT_REPOSITORY https://github.com/catchorg/Catch2.git
         GIT_TAG devel
     )
-    FetchContent_MakeAvailable(Catch2)
 
+    FetchContent_MakeAvailable(Catch2)
     list(APPEND CMAKE_MODULE_PATH "${Catch2_SOURCE_DIR}/extras")
 endif()
 
@@ -70,5 +67,4 @@ set(
 
 add_executable(tds-unit-tests ${TDS_UNIT_TESTS_SOURCES})
 target_link_libraries(tds-unit-tests PRIVATE Catch2::Catch2WithMain tds-dev Threads::Threads)
-target_include_directories(tds-unit-tests PRIVATE tests/unit)
 catch_discover_tests(tds-unit-tests TEST_PREFIX "UNIT::")
